c1dc69c9aa4f671a61618fcc6604c718c4eb9c52
Bertrick Lappa <bertricklappa@gmail.com>
2020-01-14 18:04:10 -0500

    template project

--- frontendmaster/src/app/app-routing.module.ts
+++ frontendmaster/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ClientComponent } from './client/client.component';
+import { ReceptionistComponent } from './receptionist/receptionist.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'login', component : LoginComponent},
+  {path : 'welcome', component : WelcomeComponent},
+  {path : 'client', component : ClientComponent},
+  {path : 'receptionist', component : ReceptionistComponent},
+  {path : 'doctor', component : DoctorComponent},
+  {path : 'contact', component : ContactComponent},
+  {path : 'about', component : AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- frontendmaster/src/app/app.module.ts
+++ frontendmaster/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ReceptionistComponent } from './receptionist/receptionist.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ClientComponent } from './client/client.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ReceptionistComponent,
+    DoctorComponent,
+    ContactComponent,
+    AboutComponent,
+    ClientComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
