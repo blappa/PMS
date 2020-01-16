d1771a5b28b5336767517b596ad12bf5360c82e8
Nicoleta <nicoleta.platniuc@gmail.com>
2020-01-16 15:43:59 -0500

    doctor view appointmets

--- frontendmaster/src/app/component/doctor/link7/link7.component.ts
+++ frontendmaster/src/app/component/doctor/link7/link7.component.ts
@@ -12,10 +12,34 @@ import { Injectable } from '@angular/core';
   styleUrls: ['./link7.component.css','../doctor.component.css']
 })
 export class Link7Component implements OnInit {
+  boolVar : boolean = true;
+  f_name :String;
+  l_name :String;
+  time :String;
+  appointment_type :String;
+
+  instructions : String;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  patientInfo(){
+    // make a reference to the patient information
+    this.boolVar = false;
+    console.log("check row click");
+  }
+
+  cancelAppointment(){
+    //cancel an appointment
+    
+    console.log("Canceled");
+  }
+
+  sendToReceptionist(){
+    this.instructions = '';
+    console.log("Sent to receptionist");
   }
 
 }
