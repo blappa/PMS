e9b246a41e44bdb75518514a1129a7b57bb91f5d
Bertrick Lappa <bertricklappa@gmail.com>
2020-01-19 14:29:15 -0500

    merge 7

--- backendmaster/src/main/java/com/project2/controllers/AppointmentController.java
+++ backendmaster/src/main/java/com/project2/controllers/AppointmentController.java
@@ -3,6 +3,7 @@ package com.project2.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project2.entities.Appointment;
 import com.project2.service.AppointmentService;
 
+
+
+//@CrossOrigin(origins = "http://sitemedpark.s3-website-us-east-1.amazonaws.com/")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping({"/portal"})
 public class AppointmentController {
 	
 	@Autowired
