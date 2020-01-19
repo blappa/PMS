d5a482ae1d32354d7d77578298b50e8a42b99df8
Bertrick Lappa <bertricklappa@gmail.com>
2020-01-19 16:11:57 -0500

    merge 8

--- backendmaster/src/main/java/com/project2/entities/Schedule.java
+++ backendmaster/src/main/java/com/project2/entities/Schedule.java
@@ -1,9 +1,13 @@
 package com.project2.entities;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Schedule {
@@ -15,6 +19,9 @@ public class Schedule {
 	private String dates;
 	private String time;
 	private String status;
+	
+    @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL)
+	private List<Appointment> appointments;
 	
 	@ManyToOne
 	private Hospital_User hospital_user;
