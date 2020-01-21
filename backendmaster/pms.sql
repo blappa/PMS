9566ffc41fa27636e05e7950c4edca58fff2ad82
RDixon540 <remington.dixon@gmail.com>
2020-01-21 15:20:35 -0500

    try_again

--- backendmaster/pms.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM hospital_user;
-
-DELETE FROM hospital_user;
-commit;
-
-INSERT INTO hospital_user VALUES(hibernate_sequence.nextval,15,'bl@gmail.com', 'BERTRICK','LAPPA','admin' ,'4514565897','admin','admin');
-commit;
