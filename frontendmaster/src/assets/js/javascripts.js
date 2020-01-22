48ec20af114532fe02d621deacc316847c0131da
Bertrick Lappa <bertricklappa@gmail.com>
2020-01-21 23:07:25 -0500

    merge 13

--- frontendmaster/src/assets/js/javascripts.js
+++ frontendmaster/src/assets/js/javascripts.js
@@ -66,3 +66,36 @@ $(document).ready(function(){
 	})
 	
 	});
+
+
+function count(doc, date, tagId){
+	// Set the date we're counting down to
+	var countDownDate = new Date(date).getTime();
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+	  // Get today's date and time
+	  var now = new Date().getTime();
+	  // Find the distance between now and the count down date
+	  var distance = countDownDate - now;
+
+	  // Time calculations for days, hours, minutes and seconds
+	  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	  // If the count down is finished, write some text
+	 if (days >= 0 && days <= 14) {
+		  // Display the result in the element with id="demo"
+		  //clearInterval(x);
+		  doc.getElementById(tagId).innerHTML = 
+			  days + "d " 
+			+ hours + "h "
+		    + minutes + "m " 
+		    + seconds + "s ";
+	  }else if (days > 14) {
+		    clearInterval(x);
+		    doc.getElementById(tagId).innerHTML = "PENDING";
+	  }
+	 }, 1000);
+}
